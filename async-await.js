6cea3e99c1edc438c630b1c6c17122fef013ca4f
venkatakrishnakoneti <venkatakrishnakoneti@gmail.com>
2024-04-15 13:22:16 +0530

    fetch

--- async-await.js
+++ async-await.js
@@ -1,10 +1,11 @@
 const books = async () => {
-    try{
-        let res = await fetch("https://www.anapioficeandfire.com/api/books/")
-        let data = res
-        console.log(data)
-    }catch(error){
+    try {
+        let response = await fetch("https://www.anapioficeandfire.com/api/books/")
+        const result = await response.json();
+        console.log("Success:", result);
+    } catch (error) {
         console.log("failed to fetch", error)
-    }}
+    }
+}
 
-    books()
+books()
